8de5aece1c969542ef3bb62711579e7c20b79369
Lucas <lucas.crenais@gmail.com>
2022-05-13 14:09:46 +0200

    entity & component creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GameComponent } from './game/game.component';
+import { UserComponent } from './user/user.component';
+import { TrackComponent } from './track/track.component';
+import { PlaylistComponent } from './playlist/playlist.component';
+import { ScoreComponent } from './score/score.component';
+import { ThemeComponent } from './theme/theme.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameComponent,
+    UserComponent,
+    TrackComponent,
+    PlaylistComponent,
+    ScoreComponent,
+    ThemeComponent
   ],
   imports: [
     BrowserModule,
